1dbb2b151d9f9f0bb287fb6120459ce11c3338b1
fouadlamrini <fouad11.lamrini@gmail.com>
2026-09-06 21:43:19 +0200

    call route register in app.js

--- api/app.js
+++ api/app.js
@@ -1,8 +1,14 @@
-const express = require("express");
-const app=express();
-app.use(express.json());
+const express = require('express');
+const cookieParser = require('cookie-parser'); // ضروري باش يقرأ cookies ديال refresh token
+const authRoutes = require('./routes/authRoutes');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const app = express();
+
+// 1. Middlewares لقراءة JSON و Cookies
+app.use(express.json());
+app.use(cookieParser());
+
+// 2. Register Auth Routes
+app.use('/api/v1/auth', authRoutes);
+
 module.exports = app;
