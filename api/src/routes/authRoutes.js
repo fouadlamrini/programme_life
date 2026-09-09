fc15010773a7b6db8e859f9690ca45c7f8b40f8f
fouadlamrini <fouad11.lamrini@gmail.com>
2026-09-09 21:45:27 +0200

    feat(auth): add login route

--- api/src/routes/authRoutes.js
+++ api/src/routes/authRoutes.js
@@ -4,17 +4,17 @@ const router = express.Router();
 // 1. Middleware العام للأخطاء
 const validate = require('../middleware/validate');
 
-// 2. Validation Rules ديال Register
-const { registerRules } = require('../validations/authValidation');
+// 2. Validation Rules ديال Register و Login
+const { registerRules, loginRules } = require('../validations/authValidation');
 
-// 3. Controller ديال Register
-const { register } = require('../controllers/authController');
+// 3. Controllers ديال Register و Login
+const { register, login } = require('../controllers/authController');
 
 // ==========================================
 // AUTH ROUTES
 // ==========================================
 
-// Route الوحيد المكتوب لحد الآن
 router.post('/register', registerRules, validate, register);
+router.post('/login', loginRules, validate, login);
 
 module.exports = router;
