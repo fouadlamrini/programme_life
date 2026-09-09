ef14829d20a0a4fc769f6764a8c48a5ce238f7d4
fouadlamrini <fouad11.lamrini@gmail.com>
2026-09-09 21:44:54 +0200

    feat(auth): add login validation rules

--- api/src/validations/authValidation.js
+++ api/src/validations/authValidation.js
@@ -20,6 +20,17 @@ const registerRules = [
     .isIn(['homme', 'femme']).withMessage('الجنس يجب أن يكون homme أو femme')
 ];
 
+const loginRules = [
+  body('email')
+    .isEmail().withMessage('البريد الإلكتروني غير صحيح')
+    .normalizeEmail({ gmail_remove_dots: false }),
+
+  body('password')
+    .notEmpty().withMessage('كلمة السر مطلوبة')
+    .trim()
+];
+
 module.exports = {
-  registerRules
+  registerRules,
+  loginRules
 };
