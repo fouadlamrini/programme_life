62ed11abe8bc6634f1be559a35b4b73f7be6ce2b
fouadlamrini <fouad11.lamrini@gmail.com>
2026-09-09 22:12:59 +0200

    feat(profile): register profile routes in app

--- api/app.js
+++ api/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser'); // ضروري باش يقرأ cookies ديال refresh token
 const cors = require('cors');
 const authRoutes = require('./src/routes/authRoutes');
+const profileRoutes = require('./src/routes/profileRoutes');
 
 const app = express();
 
@@ -15,5 +16,8 @@ app.use(cors({
 
 // 2. Register Auth Routes
 app.use('/api/auth', authRoutes);
+
+// 3. Register Profile Routes
+app.use('/api/profile', profileRoutes);
 
 module.exports = app;
