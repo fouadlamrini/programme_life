af4e8bd58a482c9fb410659aa10c8aa290197e53
fouadlamrini <fouad11.lamrini@gmail.com>
2026-09-09 22:04:29 +0200

    feat(web): set up react-router with login/register/dashboard routes

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,9 +1,16 @@
-
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Register from './pages/Register'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
-    <Register />
+    <Routes>
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
   )
 }
 
